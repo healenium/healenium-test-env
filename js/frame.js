9092fbf0574947d7f9f3250800b245103ed41c46
Helen <helen_yrofeeva@epam.com>
2025-10-23 13:34:18 +0300

    nested frame

--- js/frame.js
+++ js/frame.js
@@ -14,6 +14,17 @@ function showValidate() {
     if (selectField) {
         selectField.name = 'iframe_item_changed';
     }
-    
+
+    // Change the frame title and the id of the input field in nested iframe (demo_frame2.html)
+    var iframe2 = document.getElementById('iframe_2_id');
+    if (iframe2) {
+        iframe2.title = "New title for nested frame" 
+        var iframe2Doc = iframe2.contentDocument || iframe2.contentWindow.document;
+        var nestedInputField = iframe2Doc.getElementById('iframe_2_input');
+        if (nestedInputField) {
+            nestedInputField.id = 'newId_iframe2';
+        }
+    }
+
     alert('Fields have been changed!');
 }
